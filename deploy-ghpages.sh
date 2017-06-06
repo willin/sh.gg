f1d057b7aef717c365caf074785d4f75c8c9502e
willin <willin@willin.org>
2017-06-06 16:22:36 +0800

    init travis

---
+++ deploy-ghpages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf public || exit 0;
+node_modules/hexo/bin/hexo generate
+( cd public
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "travis@nodemeatspace.com"
+ git add .
+ git commit -m "Travis-CI Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
---
+++ source/_drafts/2017-05-20-pad-remote-coding.md
@@ -0,0 +1,20 @@
+---
+title: 优雅地使用平板进行远程OS X编码开发
+date: 2017-05-20 10:33:18
+layout: post
+categories: dev
+tags: [mac,suggestion,tech]
+---
+
+> 没有OS X平板(iPad是ios系统)这个问题一直困扰着我.
+
+{% cq %} 正所谓,工欲善其事必先利其器. {% endcq %}
+
+## 准备
+
+- 一台平板(iPad/Surface或者其他)
+- 一台苹果(Mac Mini/Macbook Pro或者其他)
+- 一台路由器(需支持动态域名解析,如花生壳),后续我会完善动态域名解析功能
+
+<!-- more -->
+
